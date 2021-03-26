296c61aedacf18a020a247fc51d84db6dd80bfe9
MandorSavall <bogdan.lyamzin.d@gmail.com>
2021-03-26 19:19:14 +0200

    first commit

---
+++ example-1/app.js
@@ -0,0 +1,12 @@
+// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0Ijo0NTQyMzI0MzR9.u20qA-CCcqcHRZi8lWNPeAgBWmXViD1idnVkahJCQiw
+
+
+
+const payload = {
+    id: "455d22fgg", user: {
+        name: "Alex",
+        lastName: "Benedict"
+    }
+}
+const secret = 'For All-Father and Leman Russ!'
+
